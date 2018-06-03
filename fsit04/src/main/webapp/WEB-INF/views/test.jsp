d1598de4587df28a2a2a90d0ec9fe72d1503517d
Chad Liu <tcfshchadnt@gmail.com>
2018-06-03 12:40:27 +0800

    增加 jQuery i18n

--- fsit04/src/main/webapp/WEB-INF/views/test.jsp
+++ fsit04/src/main/webapp/WEB-INF/views/test.jsp
@@ -3,6 +3,10 @@
 <%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><!--JQUERY3.3.1-->
+		<script src="./script/jquery.i18n.properties.js"></script>
+	</head>
 	<body>
 		<table>
 			<tr><c:forEach var="column" items="${data[0] }"><th>${column }</th></c:forEach></tr>	
@@ -12,4 +16,6 @@
 			
 		</table>
 	</body>
+	<script src="./script/test.js"></script>
+	
 </html>
---
+++ fsit04/src/main/webapp/script/test.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	
+               //国际化加载属性文件
+
+	$.i18n.properties({
+
+                           name:'res',
+
+                           path:'./resource/i18n/',
+
+                           mode:'map',
+                           
+                        //   language:'zh_TW',
+                           
+                           callback: function() {// 加载成功后设置显示内容
+                        	  // $.i18n.prop('string_password')
+                              alert($.i18n.prop('site.hello'));
+
+                           }
+
+                       });
+
+        });
+
+	
+	
